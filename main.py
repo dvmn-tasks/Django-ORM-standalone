9245ebcaf671c97088e5b1ca40ea4055ffb60d22
Oleg Smedyuk <conformist.mw@gmail.com>
2021-04-17 22:23:41 +0300

    suppress print lint error

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())
+    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
